55b0cf6fc6aaa5e10165085ddcedc7a17798a847
uje2kor <deepak.kumar11@in.bosch.com>
2024-08-27 10:33:08 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from '../component/home/home.component';
+import { AboutComponent } from '../component/about/about.component';
+
+export const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent}
+
+];
